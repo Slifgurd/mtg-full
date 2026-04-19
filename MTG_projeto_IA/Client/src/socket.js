dbf7b5edd76d31a5422bf3b6e4c0f1b8221af8a5
Igor Henrique <igorhq.oficio@gmail.com>
2026-04-19 07:46:05 -0300

    Update socket.js

--- MTG_projeto_IA/Client/src/socket.js
+++ MTG_projeto_IA/Client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Se estiver em produção, usa a URL do ambiente, senão usa localhost
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+//const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
 
 export const socket = io(URL);
