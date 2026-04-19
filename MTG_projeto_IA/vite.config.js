8e65c719f2900fd76fffaa85c5e13113a99f5fc3
Igor Henrique <igorhq.oficio@gmail.com>
2026-04-19 10:26:15 -0300

    Update vite.config.js

--- MTG_projeto_IA/vite.config.js
+++ MTG_projeto_IA/vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Garante que o resultado vá para a pasta 'dist' que o server.js lê
-  }
+  base: './', // ou '/'
+  plugins: [react()]
 })
