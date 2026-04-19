5cd82f3acafdd473f60c0bde5470ceac8444f500
Igor Henrique <igorhq.oficio@gmail.com>
2026-04-19 07:26:42 -0300

    Update server.js

--- MTG_projeto_IA/Server/server.js
+++ MTG_projeto_IA/Server/server.js
@@ -108,4 +108,8 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
